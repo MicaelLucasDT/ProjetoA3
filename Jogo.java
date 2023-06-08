8bacbd078a90846828dec06433afa25b024bb71a
Micael Lucas <109772685+MicaelLucasDT@users.noreply.github.com>
2023-06-07 23:06:32 -0300

    Add files via upload

---
+++ Jogo.java
@@ -0,0 +1,44 @@
+package Entity;
+
+public class Jogo {
+
+	private long id_jogo;
+	private String nome_jogo;
+	private String ano_lancamento;
+	private long id_empresa;
+	
+	
+	
+	public long getId_jogo() {
+		return id_jogo;
+	}
+	public void setId_jogo(long id_jogo) {
+		this.id_jogo = id_jogo;
+	}
+	public String getNome_jogo() {
+		return nome_jogo;
+	}
+	public void setNome_jogo(String nome_jogo) {
+		this.nome_jogo = nome_jogo;
+	}
+	public String getAno_lancamento() {
+		return ano_lancamento;
+	}
+	public void setAno_lancamento(String ano_lancamento) {
+		this.ano_lancamento = ano_lancamento;
+	}
+	public long getId_empresa() {
+		return id_empresa;
+	}
+	public void setId_empresa(long id_empresa) {
+		this.id_empresa = id_empresa;
+	}
+    
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ PlataformaJogo.java
@@ -0,0 +1,26 @@
+package Entity;
+
+public class PlataformaJogo {
+
+	private long id_jogo;
+	private long id_plataforna;
+	
+	
+	
+	
+	public long getId_jogo() {
+		return id_jogo;
+	}
+	public void setId_jogo(long id_jogo) {
+		this.id_jogo = id_jogo;
+	}
+	public long getId_plataforna() {
+		return id_plataforna;
+	}
+	public void setId_plataforna(long id_plataforna) {
+		this.id_plataforna = id_plataforna;
+	}
+	
+	
+	
+}
